06e60b7226faecd5aba0e37a4da66c5f209cc53d
Bo Zhu <zhubo03@gmail.com>
2012-06-21 18:55:56 -0400

    added MIT license

--- aes.py
+++ aes.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python
 
 
+"""
+    Copyright (C) 2012 Bo Zhu http://about.bozhu.me
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+"""
+
+
 def add_round_key(s, k):
     for i in range(4):
         for j in range(4):
@@ -45,10 +68,12 @@ InvSbox = (
     0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77, 0xD6, 0x26, 0xE1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D,
 )
 
+
 def sub_bytes(s):
     for i in range(4):
         for j in range(4):
             s[i][j] = Sbox[s[i][j]]
+
 
 def inv_sub_bytes(s):
     for i in range(4):
@@ -61,6 +86,7 @@ def shift_rows(s):
     s[0][2], s[1][2], s[2][2], s[3][2] = s[2][2], s[3][2], s[0][2], s[1][2]
     s[0][3], s[1][3], s[2][3], s[3][3] = s[3][3], s[0][3], s[1][3], s[2][3]
 
+
 def inv_shift_rows(s):
     s[0][1], s[1][1], s[2][1], s[3][1] = s[3][1], s[0][1], s[1][1], s[2][1]
     s[0][2], s[1][2], s[2][2], s[3][2] = s[2][2], s[3][2], s[0][2], s[1][2]
@@ -69,6 +95,7 @@ def inv_shift_rows(s):
 
 # learnt from http://cs.ucsb.edu/~koc/cs178/projects/JT/aes.c
 xtime = lambda a: (((a << 1) ^ 0x1B) & 0xFF) if (a & 0x80) else (a << 1)
+
 
 def mix_single_column(a):
     # please see Sec 4.1.2 in The Design of Rijndael
@@ -79,9 +106,11 @@ def mix_single_column(a):
     a[2] ^= t ^ xtime(a[2] ^ a[3])
     a[3] ^= t ^ xtime(a[3] ^ u)
 
+
 def mix_columns(s):
     for i in range(4):
         mix_single_column(s[i])
+
 
 def inv_mix_columns(s):
     # see Sec 4.1.3 in The Design of Rijndael
@@ -128,6 +157,7 @@ def text2matrix(text):
             matrix[i / 4].append(byte)
     return matrix
 
+
 def matrix2text(matrix):
     text = 0
     for i in range(4):
@@ -168,7 +198,7 @@ class AES:
 
     def encrypt(self, plaintext):
         self.plain_state = text2matrix(plaintext)
-        
+
         add_round_key(self.plain_state, self.round_keys[:4])
 
         for i in range(1, 10):
@@ -221,4 +251,3 @@ if __name__ == '__main__':
         print 'correct!'
     else:
         print 'wrong...'
-    
